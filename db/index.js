517cfc4d4eee4ebada867115e84ff6bd190b2343
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-05-30 12:12:47 +0200

    added db connection

---
+++ db/index.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+let config = {
+	user: 'postgres',
+	host: '127.0.0.1',
+	database: 'gallery',
+	password: '12345',
+	port: '5432'
+};
+
+const pool = new Pool(config);
+
+
+module.exports = {
+	query: (text, params, callback) => {
+		return pool.query(text, params, callback);
+	},
+	end: () => { return pool.end() }
+}
+
+
+// create table
+// db.query("CREATE TABLE users(user_id SERIAL PRIMARY KEY, first_name VARCHAR(500) NOT NULL,	last_name VARCHAR(500) NOT NULL,	address VARCHAR(500),	email VARCHAR(500) UNIQUE NOT NULL,	password VARCHAR(100) NOT NULL,	isAdmin Boolean,	Date_time TIMESTAMPTZ)")
+// .then(res => console.log(res))
+// .catch(error => console.log(error));
+
