ee638039098198320262c7eab442f7c2e225415f
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-05-30 12:06:37 +0200

    Added Signup, db connection and routes

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const cors = require("cors");
 
 const app = express();
 
+
+//middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+//Routes
 const auth = require("./router/auth.js");
 app.use('/auth', auth);
+
+//static files
 app.use('/', express.static('public'));
 
 let port = process.env.PORT || 3000;
