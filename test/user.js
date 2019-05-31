f77b6450ad6d5125e577a578ed4a18e9ef584970
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-05-31 08:16:59 +0200

    Change on test

--- test/user.js
+++ test/user.js
@@ -5,65 +5,78 @@ const should = require('chai').should();
 
 chai.use(chaiHttp);
 
-const userTest = { 
-	// "first_name": 'Eric',
+//register new user
+const newUser = { 
+	"first_name": 'Eric',
 	"last_name": 'Dasmony',
 	"address": 'kg 543 st',
-	"email": 'test@gmail.com',
+	"email": 'newuser@gmail.com',
 	"password": "12341",
 }
 
+const emailAlreadyTaken = { 
+	"first_name": 'Eric',
+	"last_name": 'Dasmony',
+	"address": 'kg 543 st',
+	"email": 'eric@gmail.com',
+	"password": "12341",
+}
+
+const inputValidation = { 
+	// "first_name": 'Eric',
+	"last_name": 'Dasmony',
+	"address": 'kg 543 st',
+	"email": 'eric@gmail.com',
+	"password": "12341",
+}
+
+
 describe('Registration form test', () => {
-	/*it("should register a new user", (done) => {
+	it("should register a new user", (done) => {
 		chai.request("http://localhost:3000")
 		.post('/auth/register')
-		.send(userTest)
+		.send(newUser)
 		.end((req, res) => {
-			console.log(res.body);
    expect(res).to.have.status(201);
   })
 		done();
-	});*/
+	});
 
-	/*it("should return successfuly message", (done) => {
+	it("should return successfuly message", (done) => {
 		chai.request("http://localhost:3000")
 		.post('/auth/register')
-		.send(userTest)
+		.send(newUser)
 		.end((req, res) => {
-			console.log(res.body);
    expect(res.body).to.have.property("message", "registered successfuly");
  	});
  	done();
- });*/
+ });
 
-/*it("should return error email already taken message", (done) => {
+it("should return error email already taken message", (done) => {
 	chai.request("http://localhost:3000")
 	.post('/auth/register')
-	.send(userTest)
+	.send(emailAlreadyTaken)
 	.end((req, res) => {
-		console.log(res.body);
 		expect(res.body).to.have.property("error", "email already taken");
 	});
 	done();
-});*/
+});
 
-/*it("should validate and return input status code", (done) => {
+it("should validate and return input status code", (done) => {
 	chai.request("http://localhost:3000")
 	.post('/auth/register')
-	.send(userTest)
+	.send(emailAlreadyTaken)
 	.end((req, res) => {
-		console.log(res.body);
 		(res).should.have.status(400);
 	});
 	done();
-});*/
+});
 
 it("should validate first_name ", (done) => {
 	chai.request("http://localhost:3000")
 	.post('/auth/register')
-	.send(userTest)
+	.send(inputValidation)
 	.end((req, res) => {
-		console.log(res.body);
 		(res.body).should.have.property("error", '"first_name" is required');
 	});
 	done();
@@ -72,10 +85,9 @@ it("should validate first_name ", (done) => {
 it("should length to be greater than zero", (done) => {
 	chai.request("http://localhost:3000")
 	.post('/auth/register')
-	.send(userTest)
+	.send(inputValidation)
 	.end((req, res) => {
-		console.log(res.body);
-		(userTest.last_name).should.have.lengthOf(5);
+		(inputValidation.last_name).should.have.lengthOf(5);
 	});
 	done();
 });
