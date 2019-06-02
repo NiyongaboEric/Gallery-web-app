3c66ceaf979ec63701d18664906f59f3dec1c5e8
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-06-02 14:11:48 +0200

    Require server into test 3

--- test/user.js
+++ test/user.js
@@ -11,7 +11,7 @@ const newUser = {
 	"first_name": 'new',
 	"last_name": 'user',
 	"address": 'kg 543 st',
-	"email": 'newuser14@gmail.com',
+	"email": 'newuser5@gmail.com',
 	"password": "12341",
 }
 
@@ -19,7 +19,7 @@ const emailAlreadyTaken = {
 	"first_name": 'Eric',
 	"last_name": 'Dasmony',
 	"address": 'kg 543 st',
-	"email": 'eric@gmail.com',
+	"email": 'newuser1@gmail.com',
 	"password": "12341",
 }
 
@@ -43,53 +43,53 @@ describe('Registration form test', () => {
 		done();
 	});
 
-	// it("should return successfuly message", (done) => {
-	// 	chai.request("http://localhost:3000")
-	// 	.post('/auth/register')
-	// 	.send(newUser)
-	// 	.end((req, res) => {
- //   expect(res.body).to.have.property("message", "registered successfuly");
- // 	});
- // 	done();
- // });
+	it("should return successfuly message", (done) => {
+		chai.request("http://localhost:3000")
+		.post('/auth/register')
+		.send(newUser)
+		.end((req, res) => {
+   expect(res.body).to.have.property("message", "registered successfuly");
+ 	});
+ 	done();
+ });
 
-	// it("should return error email already taken message", (done) => {
-	// 	chai.request("http://localhost:3000")
-	// 	.post('/auth/register')
-	// 	.send(emailAlreadyTaken)
-	// 	.end((req, res) => {
-	// 		expect(res.body).to.have.property("error", "email already taken");
-	// 	});
-	// 	done();
-	// });
+	it("should return error email already taken message", (done) => {
+		chai.request("http://localhost:3000")
+		.post('/auth/register')
+		.send(emailAlreadyTaken)
+		.end((req, res) => {
+			expect(res.body).to.have.property("error", "email already taken");
+		});
+		done();
+	});
 
-	// it("should return email already taken status code", (done) => {
-	// 	chai.request("http://localhost:3000")
-	// 	.post('/auth/register')
-	// 	.send(emailAlreadyTaken)
-	// 	.end((req, res) => {
-	// 		(res).should.have.status(400);
-	// 	});
-	// 	done();
-	// });
+	it("should return email already taken status code", (done) => {
+		chai.request("http://localhost:3000")
+		.post('/auth/register')
+		.send(emailAlreadyTaken)
+		.end((req, res) => {
+			(res).should.have.status(400);
+		});
+		done();
+	});
 
-	// it("should validate first_name ", (done) => {
-	// 	chai.request("http://localhost:3000")
-	// 	.post('/auth/register')
-	// 	.send(inputValidation)
-	// 	.end((req, res) => {
-	// 		(res.body).should.have.property("error", '"first_name" is required');
-	// 	});
-	// 	done();
-	// });
+	it("should validate first_name ", (done) => {
+		chai.request("http://localhost:3000")
+		.post('/auth/register')
+		.send(inputValidation)
+		.end((req, res) => {
+			(res.body).should.have.property("error", '"first_name" is required');
+		});
+		done();
+	});
 
-	// it("should length no to be zero", (done) => {
-	// 	chai.request("http://localhost:3000")
-	// 	.post('/auth/register')
-	// 	.send(inputValidation)
-	// 	.end((req, res) => {
-	// 		(inputValidation.last_name).should.have.lengthOf(5);
-	// 	});
-	// 	done();
-	// });
+	it("should length no to be zero", (done) => {
+		chai.request("http://localhost:3000")
+		.post('/auth/register')
+		.send(inputValidation)
+		.end((req, res) => {
+			(inputValidation.last_name).should.have.lengthOf(5);
+		});
+		done();
+	});
 });
