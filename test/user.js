5d06f69ed2fd747d060bab044c383a3ce7034a66
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-06-02 14:34:00 +0200

    Require server into test 7

--- test/user.js
+++ test/user.js
@@ -11,7 +11,7 @@ const newUser = {
 	"first_name": 'new',
 	"last_name": 'user',
 	"address": 'kg 543 st',
-	"email": 'newuser8@gmail.com',
+	"email": 'newuser5@gmail.com',
 	"password": "12341",
 }
 
