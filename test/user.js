742e838bfb9008e6e1891e633efff4a943457452
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-02 11:57:57 +0000

    Deploy NiyongaboEric/Gallery-web-app to github.com/NiyongaboEric/Gallery-web-app.git:gh-pages

--- test/user.js
+++ test/user.js
@@ -3,6 +3,7 @@ const chaiHttp = require('chai-http');
 const expect = require('chai').expect;
 const should = require('chai').should();
 const assert = require('chai').assert;
+const server = require("../server.js");
 
 chai.use(chaiHttp);
 
@@ -10,7 +11,7 @@ const newUser = {
 	"first_name": 'new',
 	"last_name": 'user',
 	"address": 'kg 543 st',
-	"email": 'newuser12@gmail.com',
+	"email": 'newuser18@gmail.com',
 	"password": "12341",
 }
 
@@ -32,70 +33,15 @@ const inputValidation = {
 
 
 describe('Registration form test', () => {
-		it("should register a new user", (done) => {
-			chai.request("http://localhost:3000")
-			.post('/auth/register')
-			.send(newUser)
-			.end((req, res) => {
-	   expect("foo").to.have.lengthOf(3);
-	  });
-			done();
-
-		});
-		it("should register a new user", (done) => {
-			chai.request("http://localhost:3000")
-			.post('/auth/register')
-			.send(newUser)
-			.end((req, res) => {
-	   "foo".should.have.lengthOf(3);
-	  });
-			done();
-		});
-
-		it("should register a new user", (done) => {
-			chai.request("http://localhost:3000")
-			.post('/auth/register')
-			.send(newUser)
-			.end((req, res) => {
-	   assert.lengthOf("foo", 3);
-	  });
-			done();
-		});	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// it("should register a new user", (done) => {
-	// 	chai.request("http://localhost:3000")
-	// 	.post('/auth/register')
-	// 	.send(newUser)
-	// 	.end((req, res) => {
- //   expect(res).to.have.status(201);
- //  });
-	// 	done();
-	// });
+	it("should register a new user", (done) => {
+		chai.request("http://localhost:3000")
+		.post('/auth/register')
+		.send(newUser)
+		.end((req, res) => {
+   expect(res).to.have.status(201);
+  });
+		done();
+	});
 
 	// it("should return successfuly message", (done) => {
 	// 	chai.request("http://localhost:3000")
