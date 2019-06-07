d0bd583aace82b3d58d21d52456a5045de78def0
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-06-07 13:54:23 +0200

    Online route is added and branch renamed

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(cors());
 
 //Routes
 const auth = require("./router/auth.js");
+const auth = require("./router/online-auth.js");
+
 app.use('/auth', auth);
+app.use('/online-auth', auth);
 
 //static files
 app.use('/', express.static('public'));
