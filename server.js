095b81f9e9e1cf42a9ca44add9826977f8a6e0c8
Eric NIYONGABO <niyongaboeric69@gmail.com>
2019-06-07 14:26:42 +0200

    Online route edited

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(cors());
 
 //Routes
 const auth = require("./router/auth.js");
-const auth = require("./router/online-auth.js");
+const online_auth = require("./router/online-auth.js");
 
 app.use('/auth', auth);
-app.use('/online-auth', auth);
+app.use('/online-auth', online_auth);
 
 //static files
 app.use('/', express.static('public'));
